be58304af63b108855af02e282009d676e601424
Азевич Даниил <dev@dazevich.ru>
2024-10-17 20:50:56 +0200

    feat: добавить загрузку шеринга и центрирование контента после зума.

--- ImageFeed/Presentation/SingleImage/SingleImageViewController.swift
+++ ImageFeed/Presentation/SingleImage/SingleImageViewController.swift
@@ -17,10 +17,10 @@ class SingleImageViewController : UIViewController {
         }
     }
     
-    @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var shareButton: UIButton!
-    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet private var scrollView: UIScrollView!
+    @IBOutlet private var imageView: UIImageView!
+    @IBOutlet private var shareButton: UIButton!
+    @IBOutlet private var activityIndicator: UIActivityIndicatorView!
     
     
     @IBAction func didTapBackButton(_ sender: Any) {
@@ -28,26 +28,20 @@ class SingleImageViewController : UIViewController {
     }
     
     @IBAction func didTapShareButton(_ sender: Any) {
-        guard image != nil else {
-            return
-        }
-        shareButton.isHidden = true
-        activityIndicator.startAnimating()
-        activityIndicator.isHidden = false
-        
-        let shareActivity = UIActivityViewController(activityItems: [image!], applicationActivities: nil)
+        guard let image else { return }
+       
+        let shareActivity = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        setShareButtonLoading(isLoading: true)
         DispatchQueue.main.async {
             self.present(shareActivity, animated: true) {
-                self.activityIndicator.stopAnimating()
-                self.activityIndicator.isHidden = true
-                self.shareButton.isHidden = false
+                self.setShareButtonLoading(isLoading: false)
             }
         }
     }
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setShareButtonLoading(isLoading: false)
         activityIndicator.isHidden = true
         imageView.image = image
         imageView.frame.size = image.size
@@ -57,9 +51,18 @@ class SingleImageViewController : UIViewController {
         scrollView.showsHorizontalScrollIndicator = false
         rescaleAndCenterImageInScrollView(image: image)
     }
+    
+    func setShareButtonLoading(isLoading: Bool) {
+        shareButton.isHidden = isLoading
+        activityIndicator.startAnimating()
+        activityIndicator.isHidden = !isLoading
+    }
+    
 }
 
 extension SingleImageViewController: UIScrollViewDelegate {
+    static let ZeroInset = CGFloat(0)
+    
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         return imageView
     }
@@ -79,9 +82,15 @@ extension SingleImageViewController: UIScrollViewDelegate {
         let scale = min(maxZoomScale, max(minZoomScale, min(hScale, vScale)))
         scrollView.setZoomScale(scale, animated: true)
         let newContentSize = scrollView.contentSize
-        let x = (newContentSize.width - visibleRectSize.width) / 2
-        let y = (newContentSize.height - visibleRectSize.height) / 2
-        scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
+        let verticalInset = (visibleRectSize.height - newContentSize.height) / 2
+        scrollView.contentInset = UIEdgeInsets(
+            top: verticalInset,
+            left: Self.ZeroInset,
+            bottom: verticalInset,
+            right: Self.ZeroInset
+        )
         scrollView.layoutIfNeeded()
+        
+        
     }
 }
